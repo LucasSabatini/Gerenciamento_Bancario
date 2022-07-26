7e9ac69f10f87289829226dafdd1763261034697
lusab <lucas_sabatini@hotmail.com>
2022-07-25 23:22:06 -0300

    Implementação do menu de criação de conta e da opção para escolher um tipo específico de conta a ser contratada.

--- Bank Management System/src/Bank/Management/System/BankAccount.java
+++ Bank Management System/src/Bank/Management/System/BankAccount.java
@@ -1,19 +1,21 @@
 package Bank.Management.System;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class BankAccount{
     String username;
     String accountPassword;
+    char accountType;
     double balance = 100;
     double[] prevTrans = new double[5];
 
     //Create Account
-    public BankAccount(String username, String accountPassword){
+    public BankAccount(String username, String accountPassword, char accountType){
         this.username = username;
         this.accountPassword = accountPassword;
+        this.accountType = accountType;
     }
-
 
     //Show account balance
     protected void showBalance() {
@@ -43,6 +45,8 @@ public class BankAccount{
         }
     }
 
+    //Extrato das últimas 5 transações
+
     //Realizar transferências
 
     //Tipo de conta (corrente, poupança, universitária...)
@@ -54,7 +58,7 @@ public class BankAccount{
     //Juros de cheque especial
 
     //Account Menu
-    protected void menu(){
+    protected void menu() throws IOException {
         Scanner sc = new Scanner(System.in);
         char option;
 
@@ -67,31 +71,48 @@ public class BankAccount{
             System.out.println("c) - Fazer um depósito");
             System.out.println("d) - Exibir últimas 5 transações");
             System.out.println("e) - Alterar limite de cheque especial");
-            System.out.println("f) - Exibir detalhes da conta");
-            System.out.println("g) - Alterar o tipo da conta");
-            System.out.println("h) - Finalizar operações.");
+            System.out.println("f) - Exibir tipo e detalhes da conta");
+            System.out.println("g) - Finalizar operações.");
             option = sc.next().charAt(0);
             switch (option){
                 case 'a':
                     showBalance();
                     System.out.println("Tecle Enter para retornar ao menu inicial...");
-                    sc.nextLine();
+                    System.in.read();
                     break;
                 case 'b':
                     System.out.print("Digite o valor do saque: R$");
                     double amtW = sc.nextDouble();
                     dowithdraw(amtW);
+                    System.out.println("Tecle Enter para retornar ao menu inicial...");
+                    System.in.read();
                     break;
                 case 'c':
                     System.out.print("Digite o valor do depósito: R$");
                     double amtD = sc.nextDouble();
                     dodeposit(amtD);
                     System.out.println("Tecle Enter para retornar ao menu inicial...");
-                    sc.nextLine();
+                    System.in.read();
+                    break;
+                case 'd':
+                    System.out.println("Tecle Enter para retornar ao menu inicial...");
+                    System.in.read();
+                    break;
+                case 'e':
+                    System.out.println("Tecle Enter para retornar ao menu inicial...");
+                    System.in.read();
+                    break;
+                case 'f':
+                    System.out.println("Tecle Enter para retornar ao menu inicial...");
+                    System.in.read();
+                    break;
+                case 'g':
+                    System.out.println("Operações encerradas. Sessão finalizada.");
                     break;
                 default:
                     System.out.println("Opção Inválida!\nDigite uma opção válida para continuar.");
+                    break;
             }
-        }while(option != 'h');
+        }while(option != 'g');
     }
 }
--- Bank Management System/src/Bank/Management/System/Main.java
+++ Bank Management System/src/Bank/Management/System/Main.java
@@ -1,17 +1,65 @@
 package Bank.Management.System;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main{
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Digite o seu Nome de Usuário e a sua Senha");
+
+        //Creating a new account
+        System.out.println("Bem-vindo, vamos criar sua conta!");
         System.out.print("Nome de Usuário: ");
         String username = sc.nextLine();
         System.out.print("Senha: ");
         String accountPassword = sc.nextLine();
-        BankAccount acc1 = new BankAccount(username, accountPassword);
-        acc1.menu();
+
+        //Choosing an account type
+        char accountType;
+        do {
+            System.out.println("Escolha o tipo de sua conta: ");
+            System.out.println("a) Conta Corrente");
+            System.out.println("b) Conta Poupança");
+            System.out.println("c) Conta Universitária");
+            accountType = sc.next().charAt(0);
+            switch (accountType) {
+                case 'a':
+                    System.out.println("Informações da Conta Corrente:");
+                    System.out.println(" - Limitado a 24 saques por mês;");
+                    System.out.println(" - Limitado a realizar 4 transferências;");
+                    System.out.println(" - Tarifa de R$14,60 ao mês;\n");
+                    System.out.println("Você deseja contratar este pacote?");
+                    System.out.println("a) Sim\nb) Não");
+                    char confirmAcc = sc.next().charAt(0);
+                    if(confirmAcc == 'a'){
+                        System.out.println("Parabéns! Você possui uma Conta Corrente.\n");
+                        System.out.println("Tecle Enter para abrir o menu de sua conta!");
+                        System.in.read();
+                    }
+                    else {
+                        accountType = 'e';
+                    }
+                    break;
+                case 'b':
+                    System.out.println("Informações da Conta Poupança:");
+                    System.out.println(" - Limitado a 8 saques por mês;");
+                    System.out.println(" - Limitado a realizar 3 transferências;");
+                    System.out.println(" - Taxa de rendimento de 0,5% ao mês;\n");
+                    break;
+                case 'c':
+                    System.out.println("Informações da Conta Universitária:");
+                    System.out.println(" - Limitado a 16 saques por mês;");
+                    System.out.println(" - Limitado a realizar 2 transferências;");
+                    System.out.println(" - Tarifa de R$4,55 ao mês;\n");
+                    break;
+                default:
+                    System.out.println("Digite uma opção válida.");
+                    break;
+                }
+            }while (accountType != 'a' && accountType != 'b' && accountType != 'c');
+
+        BankAccount acc1 = new BankAccount(username, accountPassword, accountType);
+        acc1.menu(); //Open account menu
 
     }
 }
